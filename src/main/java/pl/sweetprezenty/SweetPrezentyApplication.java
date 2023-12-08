ef995d4a0a8e869b2e3ecdae58a090344733cafc
TarCarbon <metalcarbon58@gmail.com>
2023-12-08 06:52:35 +0100

    first comit

---
+++ src/main/java/pl/sweetprezenty/SweetPrezentyApplication.java
@@ -0,0 +1,13 @@
+package pl.sweetprezenty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetPrezentyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetPrezentyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sweetprezenty/SweetPrezentyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sweetprezenty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetPrezentyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
